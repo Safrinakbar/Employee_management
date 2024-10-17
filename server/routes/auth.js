0c3aaf797d3c0b914756e20b6a4d2d6b48ce96ac
safrinakbar <safrinbarganaa.22cse@kongu.edu>
2024-10-17 23:10:07 +0530

    first

---
+++ server/routes/auth.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { login,verify} from '../controllers/authController.js'
+import authMiddleware from '../middleware/authMiddleware.js'
+
+
+const route =express.Router()
+
+route.post('/login', login)
+
+route.post('/verify', authMiddleware , verify)
+
+export default route;
